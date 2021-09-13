a69ea6963f194c8eccf1f3142225fb0f701c3871
Felipe Machado <felipesantos-01@hotmail.com>
2021-09-13 19:44:37 -0300

    O projeto completo

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {
+                numero: "38111",
+                name: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'38111.png',legenda:'Vereador',}
+                ]
+            },
+            {
+                numero: "01717",
+                name: 'Cleito Motoboy',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'vereador3.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: "19",
+                name: 'Zezinho Candidato',
+                partido:'ABC',
+                vice:'Cic',
+                fotos:[
+                    {url:'prefeito_1.png', legenda: 'Prefeito'},
+                    {url:'vice-prefeito1.png', legenda: 'Vice-Prefeito',small: true}
+                ]
+                
+            },
+            {
+                numero: "21",
+                name: 'Maria Porteira',
+                partido:'DEFG',
+                vice:'Cl Sin',
+                fotos:[
+                    {url:'prefeito-2.png', legenda: 'Prefeito'},
+                    {url:'vice-prefeito2.png', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
